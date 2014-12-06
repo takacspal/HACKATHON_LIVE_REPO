1a75b3b393ff751ae8ce1e1a3aa3be91e4c9e396
takacspal <smith_webster@hotmail.com>
2014-12-06 12:57:33 +0100

    Work in progress page

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
-<?php
-?>
-
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <!--<link rel="icon" href="favicon.ico">-->
+
+    <title>TheTwoPeas - Hackathon Web Application</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/evo.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <!--<link href="signin.css" rel="stylesheet">-->
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
 </head>
-<body>
-    <h1>Hackathon - Work in progress...</h1>
 
-    <p>
+    <body>
 
-    </p>
-</body>
+        <div class="container">
+
+            <form class="form-signin">
+                <h2 class="form-signin-heading-h2">TheTwoPeas</h2>
+                <h3 class="form-signin-heading-h3">Hackathon Web Application<br /><small>Work in progress...</small></h3>
+
+                <button id="enterbutton" class="btn btn-lg btn-primary btn-block" type="submit">ENTER</button>
+                <button id="eulabutton" class="btn btn-lg btn-primary btn-block" type="submit">EULA</button>
+            </form>
+
+        </div> <!-- /container -->
+
+    </body>
+
 </html>
